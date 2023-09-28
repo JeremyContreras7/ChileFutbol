08930a798a82c47e618e292a04b0330666fe58cf
JeremyContreras7 <jeremytortuguita@gmail.com>
2023-09-28 16:17:15 -0300

    actualizacion 1

--- app/src/main/java/com/example/chilefutbol/RegistroActivity.java
+++ app/src/main/java/com/example/chilefutbol/RegistroActivity.java
@@ -6,22 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistroActivity extends AppCompatActivity {
-Button boton1;
+    Button boton1;
+    EditText nombre;
+    EditText apellido;
+    EditText correo;
+    EditText contrasena;
+    Button boton2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registro);
-        boton1=findViewById(R.id.btnvolverregistro);
+        boton1 = findViewById(R.id.btnvolverregistro);
+        nombre = findViewById(R.id.txtnombre);
+        apellido = findViewById(R.id.txtapellido);
+        correo = findViewById(R.id.txtcorreo);
+        contrasena = findViewById(R.id.txtcontrasena1);
+        boton2 = findViewById(R.id.btnregistrar);
+
 
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
-
         });
     }
 }
